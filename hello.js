d884abdba2f27336862b55c4a95cd9520c4644f1
Sean Sullivan <seapasulli@uchicago.edu>
2018-08-23 09:43:33 -0500

    removed standard template from hello.js to match guide.

--- hello.js
+++ hello.js
@@ -1,18 +1,6 @@
-/*
- * hello.js
- * Copyright (C) 2018 seapasulli <seapasulli@seanslaptop.cdis>
- *
- * Distributed under terms of the MIT license.
- */
-(function(){
-  'use strict';
-  
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello Travis!\n')
 }).listen(1337, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:1337/');
-
- 
-})();
